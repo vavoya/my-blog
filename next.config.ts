600306345ce72e535087a34647080683388d2959
vavoya <vavoya6324@gmail.com>
2025-06-01 19:31:47 +0900

    Next.js 설정 업데이트: 보안 헤더(CSP) 제거 및 불필요한 변수 정리
    Nextjs 하이드레이션이랑 CSP랑 충돌

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const isDev = process.env.NODE_ENV === 'development';
-
-
 const nextConfig: NextConfig = {
   /* config options here */
     images: {
@@ -13,21 +10,6 @@ const nextConfig: NextConfig = {
                 port: '',
             },
         ],
-    },
-    async headers() {
-        return [
-            {
-                source: '/(.*)',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: isDev
-                            ? "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; object-src 'none'; base-uri 'self';"
-                            : "default-src 'self'; script-src 'self'; style-src 'self'; img-src 'self' https: data:; object-src 'none'; base-uri 'self';"
-                    },
-                ],
-            },
-        ];
     },
 };
 
