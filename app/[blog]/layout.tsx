1b02995f064cbcb9f36dc39def5b22d761ad1e83
vavoya <vavoya6324@gmail.com>
2025-06-01 19:58:33 +0900

    주석처리

--- app/[blog]/layout.tsx
+++ app/[blog]/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import getByBlogUrl from "@/fetch/server/userInfo/getByBlogUrl";
 import Header from "@/app/[blog]/_component/Header";
-import {renderError} from "@/app/_error/renderError";
+//import {renderError} from "@/app/_error/renderError";
 
 export default async function Layout({children, params}: {children: React.ReactNode, params: Promise<{blog: string}>}) {
     // 블로그 이름에 따른 유저 정보 가져오기 (id, 블로그 이름)
