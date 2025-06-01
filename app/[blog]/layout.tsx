f0a3e3d97feada3f9e58579ac65c912b18ed92e4
vavoya <vavoya6324@gmail.com>
2025-06-01 20:49:55 +0900

    불필요한 `console.log` 제거 및 오류 처리 로직 수정, 주석 정리

--- app/[blog]/layout.tsx
+++ app/[blog]/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import getByBlogUrl from "@/fetch/server/userInfo/getByBlogUrl";
 import Header from "@/app/[blog]/_component/Header";
-//import {renderError} from "@/app/_error/renderError";
+import {renderError} from "@/app/_error/renderError";
 
 export default async function Layout({children, params}: {children: React.ReactNode, params: Promise<{blog: string}>}) {
     // 블로그 이름에 따른 유저 정보 가져오기 (id, 블로그 이름)
@@ -10,8 +10,7 @@ export default async function Layout({children, params}: {children: React.ReactN
     const userInfoResponse = await getByBlogUrl(blog);
 
     if (userInfoResponse.status !== 200) {
-        return JSON.stringify(userInfoResponse, null, 2);
-        //return renderError(userInfoResponse.status, '/');
+        return renderError(userInfoResponse.status, '/');
     }
 
     return (
