f5a30ae456ae66b623ed0d0310a0c9933a282b9c
vavoya <vavoya6324@gmail.com>
2025-06-01 19:42:59 +0900

    CORS 처리 추가: 허용된 Origin 검증 및 Preflight 요청 처리 로직 구현

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,32 @@
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
+const allowedOrigins = ['https://my-blog-ten-coral-80.vercel.app', 'https://my-blog-git-dev-010-vavoyas-projects.vercel.app', 'http://localhost:3000']
+
+const corsOptions = {
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+}
+
 export default auth((request) => {
+    // Check the origin from the request
+    const origin = request.headers.get('origin') ?? ''
+    const isAllowedOrigin = allowedOrigins.includes(origin)
+
+    // Handle preflighted requests
+    const isPreflight = request.method === 'OPTIONS'
+
+    if (isPreflight) {
+        const preflightHeaders = {
+            ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
+            ...corsOptions,
+        }
+
+        return new Response(null, { status: 204, headers: preflightHeaders });
+    }
+
+
+
     const url = request.nextUrl
     const { pathname } = url;
     const session = request.auth
@@ -40,5 +65,15 @@ export default auth((request) => {
         }
     }
 
-    return NextResponse.next(); // 계속 진행
+
+    // Handle simple requests
+    const response = NextResponse.next()// 계속 진행
+
+    if (isAllowedOrigin) {
+        response.headers.set('Access-Control-Allow-Origin', origin)
+    }
+
+    Object.entries(corsOptions).forEach(([key, value]) => {
+        response.headers.set(key, value)
+    })
 });
