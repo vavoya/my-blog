7eb71b144d762d1f74111151fc207a283138850e
vavoya <vavoya6324@gmail.com>
2025-06-03 16:31:16 +0900

    TypeScript 타입 명시 추가 및 fetch 결과 처리 간소화

--- fetch/client/paginatedPost/getByFolderId.ts
+++ fetch/client/paginatedPost/getByFolderId.ts
@@ -6,18 +6,18 @@ import {PaginatedPostsResponse} from "@/models/post_info/types";
 import {path} from "@/app/api/client/paginated-posts/by-folderid/path";
 import {Response} from "@/app/api/types";
 
-export default async function getByFolderId(userId: UserInfoResponse['_id'], folderId: FolderInfoResponse['_id'], pagenum: PageNumberResult['pageNumber']) {
+export default async function getByFolderId(userId: UserInfoResponse['_id'], folderId: FolderInfoResponse['_id'], pagenum: PageNumberResult['pageNumber']): Promise<Response<PaginatedPostsResponse[]>> {
     const apiUrl = `${process.env.NEXT_PUBLIC_BASE_URL}${path}?userid=${userId}&folderid=${folderId}&pagenum=${pagenum}`;
 
     try {
         const result = await fetch(apiUrl);
-        const data: Response<PaginatedPostsResponse[]> = await result.json();
-        return data;
+        return await result.json();
     } catch (error) {
         const isNetworkError = error instanceof TypeError;
         const message = isNetworkError
             ? '통신 실패: 서버에 연결할 수 없습니다.'
             : '예상치 못한 오류가 발생했습니다.';
+
         return response.timeout(message);
     }
 }
