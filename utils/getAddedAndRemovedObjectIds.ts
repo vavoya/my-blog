247d809cc45d9e3bee8ed944ba32e00a4317ffae
vavoya <vavoya6324@gmail.com>
2025-06-11 17:40:07 +0900

    `유틸 함수 추가 및 개선: isShape 배열 타입 지원, getAddedAndRemovedObjectIds 생성`

---
+++ utils/getAddedAndRemovedObjectIds.ts
@@ -0,0 +1,18 @@
+import { ObjectId } from "mongodb";
+
+type ObjectIdLike = ObjectId | string;
+
+export const getAddedAndRemovedObjectIds = <T extends ObjectIdLike>(
+    prev: T[],
+    next: T[]
+): { add: T[]; remove: T[] } => {
+    const toStr = (id: T) => id.toString();
+
+    const prevSet = new Set(prev.map(toStr));
+    const nextSet = new Set(next.map(toStr));
+
+    const add = next.filter(id => !prevSet.has(toStr(id)));
+    const remove = prev.filter(id => !nextSet.has(toStr(id)));
+
+    return { add, remove };
+};
--- utils/isShape.ts
+++ utils/isShape.ts
@@ -4,9 +4,18 @@ export const isShape = <T extends object>(obj: any, shape: T): obj is T => {
     const objKeys = Object.keys(obj);
     const shapeKeys = Object.keys(shape);
 
+    const anyShape = shape as any;
+
     for (const key of shapeKeys) {
+        // key가 존재하지 않으면
         if (!(key in obj)) return false;
-        if (typeof obj[key] !== (shape as any)[key]) return false;
+
+        // key의 타입이 서로 다르면
+        // 배열인 경우
+        if (Array.isArray(obj[key]) && Array.isArray(anyShape[key])) {
+            const arrayType = anyShape[key][0];
+            if (!(obj[key].every(item => typeof item === arrayType))) return false;
+        } else if (typeof obj[key] !== anyShape[key]) return false;
     }
 
     for (const key of objKeys) {
