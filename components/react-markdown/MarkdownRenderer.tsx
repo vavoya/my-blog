37d847a7b8e54e3f127e3a1c7a3b190bd95ba0d6
vavoya <vavoya6324@gmail.com>
2025-06-19 17:29:11 +0900

    refactor(react-markdown): 불필요한 스타일 import 제거
    - `highlight.js/styles/atom-one-dark.css` import 제거로 코드 정리 및 빌드 최적화.

--- components/react-markdown/MarkdownRenderer.tsx
+++ components/react-markdown/MarkdownRenderer.tsx
@@ -4,8 +4,6 @@
 import React, {useEffect} from 'react'
 import {shikiPromise, parseBlocks, RootBlockNode} from "md-ast-parser"
 
-
-import 'highlight.js/styles/atom-one-dark.css'
 import 'github-markdown-css/github-markdown.css'
 import {renderBlockNode} from "@/components/react-markdown/block-node/renderBlockNode";
 
