940ef6bc91380050c8b021386b5f95fedb82a4ac
vavoya <vavoya6324@gmail.com>
2025-06-19 18:11:30 +0900

    fix(users): API 요청 캐싱 정책 수정
    - `cache` 옵션을 `force-cache`에서 `no-cache`로 변경하여 최신 데이터 보장

--- fetch/server/users/getByBlogUrl.ts
+++ fetch/server/users/getByBlogUrl.ts
@@ -12,8 +12,7 @@ export default async function getByBlogUrl(blogUrl: UserInfoResponseByBlogUrl['b
                 "Content-Type": "application/json",
                 "x-internal-secret": process.env.INTERNAL_API_SECRET!,
             },
-            cache: "force-cache",
-            next: { tags: ['all', blogUrl] }
+            cache: "no-cache",
         });
         const data: Response<UserInfoResponseByBlogUrl> = await result.json();
         return data;
