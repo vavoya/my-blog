ba3c3ef2ea4a645c9c759becda957b2ce0f1abe6
vavoya <vavoya6324@gmail.com>
2025-06-22 21:09:33 +0900

    feat(components): 에러 메시지 객체 export 처리
    - `ERROR_MESSAGE` 객체를 export하여 외부에서 재사용 가능하도록 수정

--- components/img/Img.tsx
+++ components/img/Img.tsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import Image from "next/image";
 import {isValidUrl} from "@/utils/isValidUrl";
 
-const ERROR_MESSAGE = {
+export const ERROR_MESSAGE = {
     INVALID_URL: "유효하지 않은 url 입니다.",
     LOADING: "이미지를 불러올 수 없습니다.",
 }
