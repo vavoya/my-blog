b8010b5270da690af816fe66028e2934f03b5e68
vavoya <vavoya6324@gmail.com>
2025-06-29 14:21:00 +0900

    refactor(data-access): 데이터 액세스 함수 삭제 및 주석 추가
    - getFolderInfoByUserId 함수 삭제로 불필요한 코드 제거
    - findOneAndUpdateAboutInfoByUserId 함수에 주석 추가
    - 함수 동작 및 매개변수 설명 보강으로 가독성 향상

--- data-access/about-info/findOneAndUpdateAboutInfoByUserId.ts
+++ data-access/about-info/findOneAndUpdateAboutInfoByUserId.ts
@@ -4,7 +4,17 @@ import {COLLECTION_ABOUT, DB} from "@/lib/mongoDB/const";
 import {AboutInfoDocument} from "@/lib/mongoDB/types/documents/aboutInfo.type";
 import {ClientSession} from "mongodb";
 
-
+/**
+ * 주어진 userId에 해당하는 소개글 문서를 찾아 주어진 필드로 갱신합니다.
+ *
+ * MongoDB 트랜잭션 session이 선택적으로 사용되며,
+ * `findOneAndUpdate`를 통해 갱신된 문서 이전 상태를 반환합니다.
+ *
+ * @param userId 업데이트할 사용자 ID
+ * @param updateFields 적용할 업데이트 필드 (소개글 일부 또는 전체)
+ * @param session 선택적 MongoDB ClientSession (트랜잭션용)
+ * @returns MongoDB findOneAndUpdate 결과 객체 (이전 문서 포함)
+ */
 export default async function findOneAboutInfoByUserId(userId: UserInfoDocument['_id'], updateFields: Partial<AboutInfoDocument>,session?: ClientSession) {
     return await client.db(DB).collection<AboutInfoDocument>(COLLECTION_ABOUT).findOneAndUpdate(
         {
