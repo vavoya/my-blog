ff139354a4c8cffc0b81ea2cad945a7d46dab800
vavoya <vavoya6324@gmail.com>
2025-07-12 02:03:23 +0900

    fix(sitemap): last_modified 필드의 ISO 8601 변환 호출 위치 수정
    - lastmod 태그의 last_modified 값 변환 호출 위치를 toISOString 내부로 이동

--- app/sitemap/[page]/route.ts
+++ app/sitemap/[page]/route.ts
@@ -16,8 +16,8 @@ export async function GET(_: any, { params }: { params: Params }) {
     // 유저 사이트맵 연결
     const sitemapEntries = sitemapPages.map(user => {
         const loc = `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap/user/${user.blog_url}/1`;
-        const lastmod = user.last_modified.toISOString()
-            ? `<lastmod>${user.last_modified}</lastmod>`
+        const lastmod = user.last_modified
+            ? `<lastmod>${user.last_modified.toISOString()}</lastmod>`
             : ""; // 없으면 생략
         return `<sitemap><loc>${loc}</loc>${lastmod}</sitemap>`;
     });
